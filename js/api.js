a58d6433bdfb8ca72da4b116f8d9f4815fc3c926
Ravi Mali <ravimalakar091@gmail.com>
2026-06-22 19:49:21 +0530

    feat: add searchMovie function

--- js/api.js
+++ js/api.js
@@ -1,12 +1,20 @@
-
-
-
 async function getTrending() {
     try{
         let response = await fetch(`${BASE_URL}trending/movie/day?api_key=${API_KEY}`)
         let data = await response.json();
         console.log(data.results[0])
         return data;
+    }catch(errer){
+        console.error(errer)
+    }
+    
+}
+
+async function searchMovie(movieName) {
+    try{
+            let response = await fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${movieName}`)
+            let movieNameData = await response.json();
+            return movieNameData
     }catch(errer){
         console.error(errer)
     }
