dea0e1d7c54c50c704b6dd500cccb849336c5997
Ravi Mali <ravimalakar091@gmail.com>
2026-06-24 17:52:59 +0530

    improme file formet and bugs

--- js/app.js
+++ js/app.js
@@ -1,70 +1,71 @@
 import { displayMovies } from "./ui.js";
-import { getTrending, searchMovie, getTopRated, getUpcoming, getMovieDetails } from "./api.js";
+import {
+  getTrending,
+  searchMovie,
+  getTopRated,
+  getUpcoming,
+  getMovieDetails,
+} from "./api.js";
 
-getTrending().then((data)=> (displayMovies(data.results)))
+getTrending().then((data) => displayMovies(data.results));
 
-let searchInput = document.getElementById("movie_search")
-let searchBtn = document.getElementById("search")
+let searchInput = document.getElementById("movie_search");
+let searchBtn = document.getElementById("search");
 
-searchBtn.addEventListener("click",(e) => {
-    let movieName = searchInput.value
-    searchMovie(movieName).then((data) => (displayMovies(data.results)))
-})
+searchBtn.addEventListener("click", (e) => {
+  let movieName = searchInput.value;
+  searchMovie(movieName).then((data) => displayMovies(data.results));
+});
 
+let trendingBtn = document.getElementById("trending");
+let topRatedBtn = document.getElementById("top-rated");
+let upcomingBtn = document.getElementById("upcoming");
 
-let trendingBtn = document.getElementById("trending")
-let topRatedBtn = document.getElementById("top-rated")
-let upcomingBtn = document.getElementById("upcoming")
+trendingBtn.addEventListener("click", function () {
+  getTrending().then((data) => displayMovies(data.results));
+});
 
+topRatedBtn.addEventListener("click", function () {
+  getTopRated().then((data) => displayMovies(data.results));
+});
 
-trendingBtn.addEventListener("click", function() {
-    getTrending().then((data)=> (displayMovies(data.results)))
-})
+upcomingBtn.addEventListener("click", function () {
+  getUpcoming().then((data) => displayMovies(data.results));
+});
 
-topRatedBtn.addEventListener("click", function() {
-    getTopRated().then((data)=> (displayMovies(data.results)))
-})
-
-upcomingBtn.addEventListener("click", function() {
-    getUpcoming().then((data)=> (displayMovies(data.results)))
-})
-
-let moviesContainer = document.getElementById("movies-container")
+let moviesContainer = document.getElementById("movies-container");
 
 moviesContainer.addEventListener("click", (e) => {
-    
-    if(e.target.classList.contains("fav-btn")) {
-        let card = e.target.closest(".movie-card")
-        let movieId = card.dataset.id
-        getMovieDetails(movieId).then(data => {
-            let favorites = JSON.parse(localStorage.getItem("favorites")) || []
-            let alreadyAdded = favorites.some(movie => movie.id === data.id)
-            if(alreadyAdded) {
-                alert("Already in favorites!")
-            } else {
-                favorites.push(data)
-                localStorage.setItem("favorites", JSON.stringify(favorites))
-                alert("Added to favorites!")
-            }
-        })
-
-    } else if(e.target.classList.contains("watch-list")) {
-        let card = e.target.closest(".movie-card")
-        let movieId = card.dataset.id
-        getMovieDetails(movieId).then(data => {
-            let watchList = JSON.parse(localStorage.getItem("watchList")) || []
-            let alreadyAdded = watchList.some(movie => movie.id === data.id)
-            if(alreadyAdded) {
-                alert("Already in watchList!")
-            } else {
-                watchList.push(data)
-                localStorage.setItem("watchList", JSON.stringify(watchList))
-                alert("Added to watchList!")
-            }
-        })
-
-    } else {
-        let movieId = e.target.closest(".movie-card").dataset.id
-        window.location.href = `details.html?id=${movieId}`
-    }
-})
+  if (e.target.classList.contains("fav-btn")) {
+    let card = e.target.closest(".movie-card");
+    let movieId = card.dataset.id;
+    getMovieDetails(movieId).then((data) => {
+      let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+      let alreadyAdded = favorites.some((movie) => movie.id === data.id);
+      if (alreadyAdded) {
+        alert("Already in favorites!");
+      } else {
+        favorites.push(data);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        alert("Added to favorites!");
+      }
+    });
+  } else if (e.target.classList.contains("watch-list")) {
+    let card = e.target.closest(".movie-card");
+    let movieId = card.dataset.id;
+    getMovieDetails(movieId).then((data) => {
+      let watchList = JSON.parse(localStorage.getItem("watchList")) || [];
+      let alreadyAdded = watchList.some((movie) => movie.id === data.id);
+      if (alreadyAdded) {
+        alert("Already in watchList!");
+      } else {
+        watchList.push(data);
+        localStorage.setItem("watchList", JSON.stringify(watchList));
+        alert("Added to watchList!");
+      }
+    });
+  } else {
+    let movieId = e.target.closest(".movie-card").dataset.id;
+    window.location.href = `details.html?id=${movieId}`;
+  }
+});
--- js/ui.js
+++ js/ui.js
@@ -1,53 +1,45 @@
-
-import {IMG_URL  } from "./config.js";
+import { IMG_URL } from "./config.js";
 
 function displayMovies(movies, showRemove = false) {
-    let container = document.getElementById("movies-container")
-   container.innerHTML = ""
+  let container = document.getElementById("movies-container");
+  container.innerHTML = "";
 
-    for(let movie of movies) {
-        let card = document.createElement("div")
-        card.className = "movie-card";
-        card.dataset.id = movie.id 
-        let title = document.createElement("h3")
-        title.textContent = movie.title
-        let image = document.createElement("img")
-        image.setAttribute("src", `${IMG_URL}${movie.poster_path}`);
-        let release_date = document.createElement("p")
-        release_date.className = "release_date"
-        release_date.textContent = movie.release_date
-        let vote_average = document.createElement("p")
-        vote_average.textContent = movie.vote_average;
-        let favBtn = document.createElement("button")
-favBtn.className = "fav-btn"
-favBtn.textContent = "❤️";
-let watchListBtn = document.createElement("button")
-watchListBtn.className = "watch-list"
-watchListBtn.textContent = "watchList"
+  for (let movie of movies) {
+    let card = document.createElement("div");
+    card.className = "movie-card";
+    card.dataset.id = movie.id;
+    let title = document.createElement("h3");
+    title.textContent = movie.title;
+    let image = document.createElement("img");
+    image.setAttribute("src", `${IMG_URL}${movie.poster_path}`);
+    let release_date = document.createElement("p");
+    release_date.className = "release_date";
+    release_date.textContent = movie.release_date;
+    let vote_average = document.createElement("p");
+    vote_average.textContent = movie.vote_average;
+    let favBtn = document.createElement("button");
+    favBtn.className = "fav-btn";
+    favBtn.textContent = "❤️";
+    let watchListBtn = document.createElement("button");
+    watchListBtn.className = "watch-list";
+    watchListBtn.textContent = "watchList";
 
-       
-        card.appendChild(image);        // 1. Image pehle
-        card.appendChild(title);        // 2. Title
-        card.appendChild(vote_average); // 3. Rating
-       card.appendChild(release_date); // 4. Date
-       card.appendChild(favBtn);       // 5. Buttons
-        card.appendChild(watchListBtn);
-        
+    card.appendChild(image); // 1. Image pehle
+    card.appendChild(title); // 2. Title
+    card.appendChild(vote_average); // 3. Rating
+    card.appendChild(release_date); // 4. Date
+    card.appendChild(favBtn); // 5. Buttons
+    card.appendChild(watchListBtn);
 
-
-
-if(showRemove) {
-    let removeBtn = document.createElement("button")
-    removeBtn.className = "remove-btn"
-    removeBtn.textContent = "❌"
-    card.appendChild(removeBtn)
-}
-
-
-container.appendChild(card)
+    if (showRemove) {
+      let removeBtn = document.createElement("button");
+      removeBtn.className = "remove-btn";
+      removeBtn.textContent = "❌";
+      card.appendChild(removeBtn);
     }
+
+    container.appendChild(card);
+  }
 }
 
-export {
-    displayMovies
-}
+export { displayMovies };
