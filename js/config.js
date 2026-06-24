0e563ba7d1b3d100ead34ad5947cc62793e8ac1d
Ravi Mali <ravimalakar091@gmail.com>
2026-06-24 17:56:09 +0530

    remove gitignor file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const API_KEY = "79eb4fd53f4c7c43802c57ef33f4d2a3";
+const BASE_URL = "https://api.themoviedb.org/3/";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+
+export {
+    API_KEY,
+    BASE_URL,
+    IMG_URL
+};
