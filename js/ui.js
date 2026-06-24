215bbecebff1e85921e8ddafd0eb1c29acd1a626
Ravi Mali <ravimalakar091@gmail.com>
2026-06-24 12:54:29 +0530

    add trending movie and button in ui section

---
+++ js/ui.js
@@ -0,0 +1,32 @@
+
+import {IMG_URL  } from "./config.js";
+
+function displayMovies(movies) {
+    let container = document.getElementById("movies-container")
+   container.innerHTML = ""
+
+    for(let movie of movies) {
+        let card = document.createElement("div")
+        card.className = "movie-card";
+        let title = document.createElement("h3")
+        title.textContent = movie.title
+        let image = document.createElement("img")
+        image.setAttribute("src", `${IMG_URL}${movie.poster_path}`);
+        let release_date = document.createElement("p")
+        release_date.className = "release_date"
+        release_date.textContent = movie.release_date
+        let vote_average = document.createElement("p")
+        vote_average.textContent = movie.vote_average
+       
+
+        card.appendChild(title);
+        card.appendChild(image);
+        card.appendChild(release_date);
+        card.appendChild(vote_average)
+        container.appendChild(card)
+    }
+}
+
+export {
+    displayMovies
+}
